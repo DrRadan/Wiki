51e3b555e80c59deac47c27e30dd626ffa128164
Niki Athanasiadou <rodoniki@gmail.com>
2017-01-07 17:35:19 -0500

    trials and tribulations

--- Wiki_death_scraper.py
+++ Wiki_death_scraper.py
@@ -12,11 +12,11 @@ if eval(year)<=2016 and eval(year)>=2007:
 
 #Useful variables for looping
     base_url = "https://en.wikipedia.org/wiki/Deaths_in_" 
-    months = ["January"]#,"February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    months = ["January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 #Useful reference variables
     wesco = ["english","welsh","irish","scottish","british", "american", "canadian", "australian","zealand" ] # Western-English speaking country of origin
-    cpp = ["tv","actor","actress","singer","star","oscar", "nobel","pulitzer","nascar", "television","film","movie","writer","cartoon", "rock", "celebrity", "hockey","football", "baseball", "nfl","nhl", "sports","hollywood"] #celebrity-prone-profession
+    cpp = ["tv","actor","actress","singer","star","oscar", "nobel","pulitzer","nascar", "television","film","movie","writer","cartoon", "rock", "legend",  "celebrity", "hockey","football", "baseball", "nfl","nhl", "sports","hollywood", "music", "hip-hop", "grammy", "guitar"] #celebrity-prone-profession
 
     #Initiate lists of values that will be extracted from Wikipedia for each year
     Day = []
@@ -36,107 +36,188 @@ if eval(year)<=2016 and eval(year)>=2007:
         
 #Assign appropriate number of days to each month (February will on ocassion have 29 days, I will miss 1 day of values. OK)        
         if month == "January" or month == "March" or month == "May" or month == "July" or month == "August" or month == "October" or month == "December":
-            #days = range(1, 32)
-            days = range(1, 10)
+            days = range(1, 32)
         elif month == "April" or month == "June" or month == "September" or month == "November":
             days = range(1, 31)
         elif month == "February":
             days = range (1, 29)
         
         for day in days:
-            Day.append(day)
-            Month.append(month)
-            Year.append(year)
-#Dig through the XML 
-            try:   #. Some years follow the 0-base and others the 1-base caounting for days of the month. First try 1-base (most)
-                temp = xmltodict.parse(data)["html"]["body"]["div"][2]["div"][2]["div"][3]["ul"][day]["li"]
+            try:   #. Some years follow the 0-base and others the 1-base caounting for days of the month. First try 0-base (logical)
+                temp = xmltodict.parse(data)["html"]["body"]["div"][2]["div"][2]["div"][3]["ul"][day-1]["li"]
                 for i in range(len(temp)):
-#Extract name 
-                    try: #.. Some years dont need the [0] in the page structure
-                        found = temp[i]["a"][0]["#text"]
-                    except: #.. I cannot find the expected syntax
-                        try:    #...
-                            found = temp[i]["a"]["#text"]
-                        except: #... I cannot find an entry even if I remove the '[0]'
-                            found = "NA"
+                    Day.append(day)
+                    Month.append(month)
+                    Year.append(year)
 
-            except: #. OK I cannot find anything with 1-base. It has to be 0-base
-                try: #. Now try the 0-base 
-                    temp = xmltodict.parse(data)["html"]["body"]["div"][2]["div"][2]["div"][3]["ul"][day-1]["li"]
-                    for i in range(len(temp)):
-#Extract name 
-                        try: #.. Some years dont need the [0] in the page structure
-                            found = temp[i]["a"][0]["#text"]
-                        except: #.. Can I find a valid entry if I remove '[0]' from 'temp[i]["a"][0]["@href"]'?
-                            try:    #...
-                                found = temp[i]["a"]["#text"]
-                            except: #... I cannot find an entry even if I remove the '[0]'
-                                found = "NA"
-                except: #.
-                    found = "NA"
-            ID.append(found) #name
-#wikipedia entry text length (words)(there will be a slight overestimation because of non-ascii characters)
-            if found != "NA":
-
-                try:
-                    wiki = WikiApi()
-                    results = wiki.find(found)
-                    article = wiki.get_article(results[0])
-                    a = article.content
-                    a = a.split()
-                    RefLength.append(len(a))
-                except: #....
-                    RefLength.append("NA")
-            else: #-
-                RefLength.append("NA")             
-
-#Extract age, Western-English speaking country of origin (WESCO), celebrity-prone-profession (CPP)
-            for i in range(len(temp)):
-                try:
-                    text =  temp[i]["#text"]
-                    Other.append(text)
-                    
-                    text = re.sub( r'\([^)]*\)', '', text) #removes everything in parethneses. It tends to be years that would confuse the script below
-                    #age
-                    a = re.findall(r'\d+',text) #finds all numbers
-                    if len(a)==1: # there is only one number returned, it has to be age (some very few errors, ~<0.1%, expected)
-                        a = int(a[0]) 
-                        Age.append(a)
-                    # what happens if there are still other numbers included in the text?
-                    elif len(a[0]) <= 3: # if the first number has a naximum of 3 digits it has to be age
-                        a = int(a[0]) 
-                        Age.append(a)
+                    ## A. Determine name of celebrity and lenth of their wikipedia entry page
+                    if isinstance(temp[i]["a"], list) == True :  #.. Some years dont need the [0] in the page structure and some not
+              			found = temp[i]["a"][0]["#text"]
+                    elif isinstance(temp[i]["a"], dict) == True :
+						found = temp[i]["a"]["#text"]
                     else:
-                        Age.append("NA") #if more than one numbers are present I cannot guarantee I can ID the age without further work
-                    
-                    #wesco & cpp
-                    a = [re.findall(x, text.lower())==[x] for x in wesco] #returns list of False/True
-                    for i in range(len(a)):
+						found = "NA"
+
+                    ID.append(found)
+                    wiki = WikiApi()
+                    try:
+                    	results = wiki.find(found)
+                        article = wiki.get_article(results[0]) #Assume 1st hit is the most relevant. Supported by empirical evidence 
+                        a = article.content
+                        a = a.split()
+                        RefLength.append(len(a))
+                    except:
+                        RefLength.append("NA")
+
+
+                    ## B. Determine values Other, Age, WESCO, and CPP within this entry of this day of this month for the year entered
+                    try: #.. Some years need the [0] in the page structure and others don't
+                        text = temp[i]["#text"]
+                        Other.append(text)
+
+                        #1. age
+                        text = re.sub( r'\([^)]*\)', '', text) #removes everything in parethneses. It tends to be years that would confuse the script below
+                        a = re.findall(r'\d+',text) #finds all numbers
+                        if len(a)==1: # there is only one number returned, it has to be age (some very few errors, ~<0.1%, expected)
+                            a = int(a[0]) 
+                            Age.append(a)
+                        # what happens if there are still other numbers included in the text?
+                        elif len(a[0]) <= 3: # if the first number has a naximum of 3 digits it has to be age
+                            a = int(a[0]) 
+                            Age.append(a)
+                        else:
+                            Age.append("NA") #if more than one numbers are present I cannot guarantee I can ID the age without further work
+
+                        #2. wesco 
+                        a = [re.findall(x, text.lower())==[x] for x in wesco] #returns list of False/True
                         test = 0
-                        if a[i] == True:
-                            test = 1
-                            break
-                    WESCO.append(test)
+                        for i in range(len(a)):
+                            if a[i] == True:
+                                test = 1
+                                break
+                        WESCO.append(test)
                     
-                    a = [re.findall(x, text.lower())==[x] for x in cpp]
-                    for i in range(len(a)):
+                        # 3. cpp
+                        a = [re.findall(x, text.lower())==[x] for x in cpp]
                         test = 0
-                        if a[i] == True:
-                            test = 1
-                            break
-                    CPP.append(test)
+                        for i in range(len(a)):
+                            if a[i] == True:
+                                test = 1
+                                break
+                        CPP.append(test)
+
+                    except: #..
+                        Other.append("NA")
+                        Age.append("NA")
+                        WESCO.append("NA")
+                        CPP.append("NA")                    
+
+            except: #. Now redo try for alternative
+                try:#. Some years follow the 0-base and others the 1-base caounting for days of the month. Now try 1-base 
+                    temp = xmltodict.parse(data)["html"]["body"]["div"][2]["div"][2]["div"][3]["ul"][day]["li"]
+                    for i in range(len(temp)):
+                        Day.append(day)
+                        Month.append(month)
+                        Year.append(year)
+
+                    ## A. Determine name of celebrity and lenth of their wikipedia entry page
+                    	if isinstance(temp[i]["a"], list) == True : #.. Some years dont need the [0] in the page structure and some not
+                        	found = temp[i]["a"][0]["#text"]
+                    	elif isinstance(temp[i]["a"], dict) == True :
+                        	found = temp[i]["a"]["#text"]
+                    	else:
+                        	found = "NA"
+
+                    	ID.append(found)
+                    	wiki = WikiApi()
+                    	try:
+                    		results = wiki.find(found)
+                        	article = wiki.get_article(results[0]) #Assume 1st hit is the most relevant. Supported by empirical evidence 
+                        	a = article.content
+                        	a = a.split()
+                        	RefLength.append(len(a))
+                    	except:
+                        	RefLength.append("NA")
+
+
+                        ## B. Determine values Other, Age, WESCO, and CPP within this entry of this day of this month for the year entered
+                        try: #.. Some years need the [0] in the page structure and others don't
+                            text = temp[i]["#text"]
+                            Other.append(text)
+
+                            #1. age
+                            text = re.sub( r'\([^)]*\)', '', text) #removes everything in parethneses. It tends to be years that would confuse the script below
+                            a = re.findall(r'\d+',text) #finds all numbers
+                            if len(a)==1: # there is only one number returned, it has to be age (some very few errors, ~<0.1%, expected)
+                                a = int(a[0]) 
+                                Age.append(a)
+                            # what happens if there are still other numbers included in the text?
+                            elif len(a[0]) <= 3: # if the first number has a naximum of 3 digits it has to be age
+                                a = int(a[0]) 
+                                Age.append(a)
+                            else:
+                                Age.append("NA") #if more than one numbers are present I cannot guarantee I can ID the age without further work
+
+                            #2. wesco 
+                            a = [re.findall(x, text.lower())==[x] for x in wesco] #returns list of False/True
+                            test = 0
+                            for i in range(len(a)):
+                                if a[i] == True:
+                                    test = 1
+                                    break
+                            WESCO.append(test)
                     
-                except:
-                    WESCO.append("NA")
+                            # 3. cpp
+                            a = [re.findall(x, text.lower())==[x] for x in cpp]
+                            test = 0
+                            for i in range(len(a)):
+                                if a[i] == True:
+                                    test = 1
+                                    break
+                            CPP.append(test)
+
+                        except: #..
+                            Other.append("NA")
+                            Age.append("NA")
+                            WESCO.append("NA")
+                            CPP.append("NA")                                        
+                except: #.
+                    Day.append(day)
+                    Month.append(month)
+                    Year.append(year)
+                    ID.append("NA")
+                    RefLength.append("NA")
                     Age.append("NA")
+                    WESCO.append("NA")
                     CPP.append("NA")
                     Other.append("NA")
-            time.sleep(1)#1 second sleep after each day of the month
-    
+
+### ~~~~~~~ Debugging Script ~~~~~~~~ ###
+
+#    print "Day: "+str(len(Day))print Day
+#    print "Month: "+str(len(Month))
+#    print Month
+#    print "Year: "+str(len(Year))
+#    print Year
+#    print "ID: "+str(len(ID))
+#    print ID
+#    print "Age: "+str(len(Age))
+#    print Age
+#    print "RefLength: "+str(len(RefLength))
+#    print RefLength
+#    print "WESCO: "+str(len(WESCO))
+#    print WESCO
+#    print "CPP: "+str(len(CPP))
+#    print CPP
+#    print "Other: "+str(len(Other))
+#    print Other
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
     temp = pd.DataFrame(
-    {'Day': Day,
-     'Month': Month,
-     'Year': Year,
+    {'Day': Day[0:len(ID)], #this takes care of a weird bug that gives one more entry to D/M/Y because of inconsistensies at the wiki html. 
+     'Month': Month[0:len(ID)],
+     'Year': Year[0:len(ID)],
      'Name': [x.encode('ascii', 'ignore') for x in ID],
      'Age': Age,
      'ImportanceIndex': RefLength,
@@ -145,13 +226,12 @@ if eval(year)<=2016 and eval(year)>=2007:
      'Text': [x.encode('ascii', 'ignore') for x in Other]
     })
     temp = temp[["Year","Month","Day","Name","Age","ImportanceIndex","WESCO","CPP","Text"]]
-    name = "V2/"+year + "_results.csv"
+    name = "V2/" + year + "_results.csv"
     temp.to_csv(name)
 
 
 else:
     print("Sorry there is some problem with the year you entered. Aborting....")
-
 
 
 
